c1001b5886abcb93967a0cab98dd2e94d888f0d0
Kenneth Quinn <kqui108@wgu.edu>
2024-04-03 10:51:44 -0500

    Created an array of SalesPerson with data.

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesPersonListComponent implements OnInit {
 
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup.kumar@gmail.com", 50000), 
+    new SalesPerson("John", "Doe", "john.doe@gamil.com", 40000),
+    new SalesPerson("Claire", "Murphy", "claire.murphy@gmail.com", 90000),
+    new SalesPerson("Mai", "Truong", "mai.truong@gmail.com", 60000)
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
